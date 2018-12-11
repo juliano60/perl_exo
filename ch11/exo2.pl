1e9b908ba3b3ad2e413510720c3a0f95833594e3
Jules Konan <juliano60@hotmail.co.uk>
2018-12-11 05:52:02 +0000

    Added exo

--- ch11/exo2.pl
+++ ch11/exo2.pl
@@ -11,4 +11,15 @@ die "Usage: $0 <year> <month>" unless @ARGV == 2;
 
 my ($year, $month) = @ARGV;
 
-say "$year, $month";
+my $tm = Time::Moment->new(
+        year    => $year,
+        month   => $month
+);
+
+my $now = Time::Moment->now;
+
+my $delta_years = $tm->delta_years($now);
+my $delta_mon = $tm->delta_months($now) % 12;
+
+printf "%d year%s, %d month%s\n", $delta_years, ($delta_years > 1 ? "s" : ""), 
+        $delta_mon, ($delta_mon > 1 ? "s" : "");
